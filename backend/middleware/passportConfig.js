1e6e634d05ae1b6920c609fd5d1607ea74cca55c
Rodney Gauna <rodneygauna@gmail.com>
2026-03-17 10:30:37 -0700

    feat: enhance JWT extraction by adding cookie support in passport configuration

--- backend/middleware/passportConfig.js
+++ backend/middleware/passportConfig.js
@@ -1,8 +1,13 @@
 import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
 import User from "../models/userModel.js";
 
+const cookieExtractor = (req) => req?.cookies?.jwt ?? null;
+
 const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  jwtFromRequest: ExtractJwt.fromExtractors([
+    cookieExtractor,
+    ExtractJwt.fromAuthHeaderAsBearerToken(),
+  ]),
   secretOrKey: process.env.JWT_SECRET,
 };
 
@@ -11,12 +16,12 @@ export default (passport) => {
     new JwtStrategy(opts, async (jwt_payload, done) => {
       try {
         const user = await User.findById(jwt_payload.id).select(
-          "-password_hash"
+          "-password_hash",
         );
         return user ? done(null, user) : done(null, false);
       } catch (error) {
         return done(error, false);
       }
-    })
+    }),
   );
 };
