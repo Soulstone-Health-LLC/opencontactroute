b6ccc7f9cf3479211527be63f01de6ddfa84976c
Rodney Gauna <rodneygauna@gmail.com>
2026-03-23 22:00:14 -0700

    fix: store user ID in _changedBy field during site configuration update

--- backend/controllers/siteConfigControllers.js
+++ backend/controllers/siteConfigControllers.js
@@ -30,8 +30,8 @@ export const updateSiteConfig = asyncHandler(async (req, res) => {
     upsert: true,
     returnDocument: "after",
     setDefaultsOnInsert: true,
+    _changedBy: req.user._id,
   });
-  config._changedBy = req.user._id;
 
   res.json(config);
 });
